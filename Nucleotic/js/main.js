145b6caff5691ac31d13cc696002d0315014e77a
QuentinBensmaine <43670995+QuentinBensmaine@users.noreply.github.com>
2019-02-26 17:06:52 +0100

    Add files via upload

--- Nucleotic/js/main.js
+++ Nucleotic/js/main.js
@@ -15,19 +15,47 @@ $(function() {
 		e.preventDefault();
 	});
 	$('#login-submit').click(function(e){
-		$('#bandeau-connected').delay(500).fadeIn(500);
-		$('#base-de-donnees').delay(500).fadeIn(500);
-		$('.container-sidebar').delay(1000).fadeIn(500);
-		$('.container-bdd').delay(1500).fadeIn(500);
+		$('#connected').delay(500).fadeIn(500);
 		$("#connexion").fadeOut(500);
 		e.preventDefault();
 	});
 	$('#logout').click(function(e){
 		$("#connexion").delay(500).fadeIn(500);
-		$('#bandeau-connected').fadeOut(500);
-		$('#base-de-donnees').fadeOut(500);
-		$('.container-sidebar').fadeOut(500);
-		$('.container-bdd').fadeOut(500);
+		$('connected').fadeOut(500);
 		e.preventDefault();
 	});
 });
+
+function myFunction() {
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById('recherche');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById("liste");
+	li = ul.getElementsByTagName('li');  
+	for (i = 0; i < li.length; i++) {
+  	a = li[i].getElementsByClassName("panel-heading")[0];
+  	txtValue = a.textContent || a.innerText;
+  	if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		li[i].style.display = "";
+  	} else {
+		li[i].style.display = "none";
+  	}
+	}
+}
+	
+var el = document.querySelector('.fas');
+document.querySelector('#increment').addEventListener('click', function(){
+		var hide = new Boolean (false);
+    let count = Number(el.getAttribute('data-count')) || 0;
+    el.setAttribute('data-count', count + 1);
+    el.classList.remove('notify');
+    el.offsetWidth = el.offsetWidth;
+		el.classList.add('notify');
+    if(hide == false){
+        el.classList.add('show-count');
+		}
+}, false);
+document.querySelector('.fas').addEventListener('click', function(){
+	el.setAttribute('data-count', count=0);
+	el.classList.remove('show-count');
+},false);
